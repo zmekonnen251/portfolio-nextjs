52a7e2b41449a4a612fba6b9cab30522ef714b22
Zelalem <mekonnen.zelalem251@gmail.com>
2022-10-20 21:44:32 +0300

    Fix problem with google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -8,14 +8,14 @@ function MyApp({ Component, pageProps }) {
 		<>
 			<Script
 				strategy='lazyOnload'
-				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+				src={'https://www.googletagmanager.com/gtag/js?id=G-K5ZZ3PNWXC'}
 			/>
 			<Script strategy='lazyOnload' id=''>
 				{`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          gtag('config', 'G-K5ZZ3PNWXC', {
                       page_path: window.location.pathname,
                   });
       `}
